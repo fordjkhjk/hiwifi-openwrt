d156bf3c9b395b1f267e3d800724b455934452c9
fordjkhjk <152050905+fordjkhjk@users.noreply.github.com>
2026-08-25 16:09:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,11 +10,24 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 修改后台默认 IP 为 192.168.112.200
+sed -i 's/192.168.1.1/192.168.112.200/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. 旁路由配置：自动网关/DNS，关闭 LAN 口 DHCP
+cat << 'EOF' >> package/base-files/files/etc/uci-defaults/99-custom-network
+uci set network.lan.gateway='192.168.112.1'
+uci del network.lan.dns
+uci add_list network.lan.dns='192.168.112.1'
+uci set dhcp.lan.ignore='1'
+uci commit network
+uci commit dhcp
+EOF
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. 精准移除导致内核卡死的摄像头坏补丁
+find target/linux/ -name "*810-uvc-add-iPassion-iP2970-support.patch*" -exec rm -rf {} \;
+
+# 4. 解决 Go 工具链版本匹配问题
+export GOTOOLCHAIN=auto
+
+# 5. 拉取第三方整合插件源
+git clone --depth=1 https://github.com/kenzok8/small-package.git package/small-package
